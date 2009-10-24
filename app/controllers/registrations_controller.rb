6ad4ae248327e62f7f634e28da869f719c958e6b
Jordan Brough <jordan@animoto.com>
2009-10-24 08:29:09 -0600

    Don't check authenticity token on registration creation
    Nothing sensitive here that could be compromised anyway.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < ApplicationController
   
   skip_before_filter :super_authenticate, :only => ["index", "new", "create", "show"]
+  skip_before_filter :verify_authenticity_token, :only => 'create'
   before_filter :authenticate, :only => "index"
   
   # GET /registrations
